d47a64ae69ec831e8f65b98638cc975250847b3a
gagle1231 <kke1231@naver.com>
2024-09-15 02:17:26 +0900

    feat: add controller

--- src/main/java/com/security/de/controller/KeyController.java
+++ src/main/java/com/security/de/controller/KeyController.java
@@ -1,4 +1,24 @@
 package com.security.de.controller;
 
+import com.security.de.service.KeyManager;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/keys")
 public class KeyController {
+
+    @GetMapping("/create")
+    public String keyPage() {
+        return "createKey";
+    }
+
+    @PostMapping("/create")
+    public ModelAndView createKey(@RequestParam String senderId) {
+        KeyManager.createKeyPair(senderId);
+        ModelAndView modelAndView = new ModelAndView("completePage");
+        modelAndView.addObject("message", senderId + "님의 키를 생성하였습니다.");
+        return modelAndView;
+    }
 }
--- src/main/java/com/security/de/controller/MessageController.java
+++ src/main/java/com/security/de/controller/MessageController.java
@@ -1,4 +1,43 @@
 package com.security.de.controller;
 
+import com.security.de.service.SendDataService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+
+import java.io.UnsupportedEncodingException;
+
+@Controller
+@RequestMapping("/messages")
+@RequiredArgsConstructor
 public class MessageController {
+
+    private final SendDataService sendDataService;
+
+    @GetMapping("/send")
+    public String sendPage() {
+        return "sendpage";
+    }
+
+    @PostMapping("/send")
+    public String sendData(@RequestParam String senderId, @RequestParam String receiverId, @RequestParam String contents, Model model) {
+        sendDataService.sendMessage(senderId, receiverId, contents);
+        String message = receiverId + "님께 암호화하여 메세지를 보냈습니다.";
+        model.addAttribute("message", message);
+        return "completePage";
+    }
+
+    @GetMapping("/read")
+    public String readPage() {
+        return "readMessage";
+    }
+
+    @PostMapping("/read")
+    public String readMessage(@RequestParam String senderId, @RequestParam String receiverId, Model model) {
+        String message = sendDataService.readMessage(senderId, receiverId);
+        model.addAttribute("message", message);
+        model.addAttribute("invalidSign", "Sign이 유효합니다.");
+        return "readMessage";
+    }
 }
